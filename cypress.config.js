39c9d367a388c99378afa0fbab015a5b804286f2
guilherme <guilhermefes@usp.br>
2023-06-14 11:45:38 -0300

    primeiro teste de cypress

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      cy.visit('https://127.0.0.1:5500')
       // implement node event listeners here
     },
+    baseUrl:'http://127.0.0.1:5500'
   },
 }); 
